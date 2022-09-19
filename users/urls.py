3e69b6e52c97648e0706ee8c696c7f882f660652
Макс Добжинецький Добжинецький <maks.dobzhinetsky14@gmail.com>
2022-09-19 21:00:30 +0300

    updated UserViewSet functionality

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,10 @@ from . import views
 
 urlpatterns = [
     path('info/', views.UserViewSet.as_view({'get': 'list'})),
+    path('info/<int:pk>/', views.UserViewSet.as_view({
+        'get': 'retrieve',
+        'delete': 'destroy'
+    })),
     path('register/', views.RegisterApi.as_view()),
     path('logout/', views.LogoutView.as_view(), name='auth_logout'),
 ]
