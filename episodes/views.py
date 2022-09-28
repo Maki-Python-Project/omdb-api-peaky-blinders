8c64000283f33e0e3e25e0a29d0cfdbdbc1fb220
Макс Добжинецький Добжинецький <maks.dobzhinetsky14@gmail.com>
2022-09-28 15:21:18 +0300

    deleted legacy code

--- episodes/views.py
+++ episodes/views.py
@@ -2,7 +2,7 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, filters, permissions
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from django.db.models.query import QuerySet
 
 from .models import Episode, Comment
